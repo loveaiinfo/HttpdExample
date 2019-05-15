72cc89b50bc9643fcb3d05fcf23095b836e98a13
Jun Liu <liujun.sun@bytedance.com>
2019-05-15 16:20:01 +0800

    tcp client

---
+++ app/src/main/java/info/loveai/httpdexample/XBotClient.java
@@ -0,0 +1,7 @@
+package info.loveai.httpdexample;
+
+// http://androidsrc.net/android-client-server-using-sockets-client-implementation/
+// oksocket
+// socket.io
+public class XBotClient {
+}
---
+++ app/src/main/java/info/loveai/httpdexample/XBotServer.java
@@ -0,0 +1,4 @@
+package info.loveai.httpdexample;
+
+public class XBotServer {
+}
